0a25ff03631361dc4261605df7f8bb873bd2be3c
sontanhoang2001 <sontanhoang2001@gmail.com>
2023-01-31 14:31:06 +0700

    update 32/01/2023

--- src/api/env.js
+++ src/api/env.js
@@ -2,11 +2,11 @@ const pathname_localhost = 'api/v1';
 const pathname_tomcat = 'toptop-1.0/api/v1';
 const pathname_socket = 'toptop-1.0';
 
-const APP_PATH_LOCALHOST = `http://localhost:8080/${pathname_localhost}`;
-const APP_PATH_TOMCAT = `http://localhost:8085/${pathname_tomcat}`;
+const APP_PATH_LOCALHOST = `http://localhost:8081/${pathname_localhost}`;
+const APP_PATH_TOMCAT = `https://494e-103-151-238-223.ap.ngrok.io/${pathname_tomcat}`;
 
-const APP_PATH_LOCALHOST_SOCKET = `http://localhost:8080/ws`;
-const APP_PATH_TOMCAT_SOCKET = `http://localhost:8085/${pathname_socket}/ws`;
+const APP_PATH_LOCALHOST_SOCKET = `http://localhost:8081/ws`;
+const APP_PATH_TOMCAT_SOCKET = `https://494e-103-151-238-223.ap.ngrok.io/${pathname_socket}/ws`;
 
 const Env = {
     APP_PATH_API: APP_PATH_TOMCAT,
